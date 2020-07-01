f462f9ad733fb1145efef8a8e76ab70dac4ccb6e
hg.king <hg_king@external.mckinsey.com>
2020-07-01 09:12:43 -0400

    starting to clean up the javascript

--- www/index.js
+++ www/index.js
@@ -17,39 +17,82 @@ const dataVizColors = [
   '#051c2c',
 ]
 
+const errorCheckElement = (elem, id) => {if (!elem) { throw new Error(`UI is expecting an element with the following id :: ${id}`)}};
+
+const parkColor = '#d1f4dd';
+
+const chartWidth = 816,
+      chartHeight = 1000;
+
+const loaderId = "loader";
+const hiddenClass = "hidden";
+
+const selectId = "fields";
+const rangeId = "date-slider";
+const select = document.getElementById(selectId);
+errorCheckElement(select, selectId);
+const range = document.getElementById(rangeId);
+errorCheckElement(range, rangeId);
+
+const mapId = "#map";
+
+const stringRegex = /[a-zA-z]/;
+const decimalRegex = /\./;
+const stringPredicate = (field) => field.match(stringRegex);
+const decimalPredicate = (field) => field.match(decimalRegex);
+
+const averageText = (v) => `Average: ${v}`;
+const sumText = (v) => `Total: ${v}`;
+const dateText = (v) => `Date: ${v}`;
+
+const mapTooltip = (record, val) => {
+  let string = '';
+  if ( record ) { string += `${record['BOROUGH_GROUP']}: ${record['NEIGHBORHOOD_NAME']}<br>`; }
+  if ( val ) { string += val; }
+  return string;
+}
+
+const getDateFromCommit = (commit) => commit.commit.committer.date.split('T')[0];
+
+
+
+//
+// UI Bits
+//
+
+// utility function to put an `tag element with `id into `parentId
+const buildElem = (parentId, id, tag) => {
+  const elem = document.createElement(tag);
+  elem.id = id;
+  document.getElementById(parentId).append(elem);
+  return elem;
+}
+
 // method to add bits to the UI
-const buildLegend = () => {
+const buildLegend = (colors) => {
   const legendId = "legend"
   // add a min value
-  const min = document.createElement("span");
-  min.id = "min";
-  document.getElementById(legendId).append(min);
+  buildElem(legendId, 'min', 'span');
   // add a colored square for every single color we have
-  dataVizColors.forEach((color) => {
-    const square = document.createElement("div");
+  colors.forEach((color, i) => {
+    const square = buildElem(legendId, `square-${i}`, 'div');
     square.classList.add("legend-block");
     square.style.backgroundColor = color;
-    document.getElementById(legendId).append(square);
   });
   // add a max value
-  const max = document.createElement("span");
-  max.id = "max";
-  document.getElementById(legendId).append(max);
+  buildElem(legendId, 'max', 'span');
 }
 
 const buildDesc = () => {
   const descId = "data";
-  // add a total value
-  const sum = document.createElement("h6");
-  sum.id = "sum";
-  document.getElementById(descId).append(sum);
-  // add an average value
-  const avg = document.createElement("h6");
-  avg.id = "avg";
-  document.getElementById(descId).append(avg);
+  ["sum", "avg", "date"].map((id) => buildElem(descId, id, 'p'))
 }
 
+
 //
+// Mathy bits
+//
+
 const asyncWait = async (count) => new Promise(resolve => setTimeout(resolve, count? count : 1000 ));
 
 function Utf8ArrayToStr(array) {
@@ -58,48 +101,57 @@ function Utf8ArrayToStr(array) {
   return s;
 }
 
-const buildUrl = (sha) => `https://raw.githubusercontent.com/nychealth/coronavirus-data/${sha}/data-by-modzcta.csv`;
-
 // given a range of min to max where the range is divided into n segments
 // and an integer x where min <= x <= max
 // let bucket be the segment number that x is within
 const buckets = (x, min, max, n) => Math.floor((x - min) / ((max - min) / n));
 
+const buildRawUrl = (sha) => `https://raw.githubusercontent.com/nychealth/coronavirus-data/${sha}/data-by-modzcta.csv`;
 const mapUrl = "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/Geography-resources/MODZCTA_2010_WGS1984.geo.json";
-const povertyUrl = "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-poverty.csv";
+// const povertyUrl = "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-poverty.csv";
 const historyUrl = "https://api.github.com/repos/nychealth/coronavirus-data/commits?path=data-by-modzcta.csv";
-const dataUrl = "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/data-by-modzcta.csv";
-// https://api.github.com/repos/nychealth/coronavirus-data/commits?path=data-by-modzcta.csv
-// https://raw.githubusercontent.com/nychealth/coronavirus-data/cf508e8fe08ddff44a847c1b54209c598b88c913/data-by-modzcta.csv
-
-const chartWidth = 860,
-      chartHeight = 1060;
-
-let data;
 
 // setup UI
-buildLegend();
+const legendColors = dataVizColors;
+buildLegend(legendColors);
 buildDesc();
 
+// these get created in the methods above
+const min = document.getElementById('min');
+const max = document.getElementById('max');
+const sum = document.getElementById('sum');
+const avg = document.getElementById('avg');
+const date = document.getElementById('date');
+
+let data;
 (async () => {
 
+  // first get all the commits for the file
   const historyDataChunk = await Dataworker.getData(historyUrl, Filetype.JSON);
   const historyData = historyDataChunk.to_object().JsonStruct;
-  const dataUrls = historyData.map((object) => buildUrl(object.sha));
+  // turn each commit sha into a raw url
+  const dataUrls = historyData.map((object) => buildRawUrl(object.sha));
 
   // Setup the data
-  let covidMapDataChunk = await Dataworker.getData(dataUrl, Filetype.CSV);
-  console.log( covidMapDataChunk instanceof Chunk);
+
+  // start by initializing the first URL into the Chunk
+  let covidMapDataChunk = await Dataworker.getData(dataUrls[0], Filetype.CSV);
   const covidMapObj = covidMapDataChunk.to_object().CsvStruct;
   const headers = covidMapDataChunk.keys();
-
-  for ( const url of dataUrls) {
-    covidMapDataChunk = await Dataworker.append(url, Filetype.CSV, covidMapDataChunk);
+  // then append each data url onto that chunk
+  // this would be easier with ranges
+  for ( let index in dataUrls) {
+    if ( ++index === dataUrls.length ) continue;
+    covidMapDataChunk = await Dataworker.append(dataUrls[index], Filetype.CSV, covidMapDataChunk);
   }
+
+  // hide our loader
+  const loader = document.getElementById(loaderId);
+  loader.classList.add(hiddenClass);
 
   // setup the map
   const nyc = await d3.json(mapUrl);
-  const map = d3.select("#map").append("svg")
+  const map = d3.select(mapId).append("svg")
       .attr("width", chartWidth)
       .attr("height", chartHeight);
 
@@ -111,7 +163,7 @@ buildDesc();
       .fitSize([chartWidth, chartHeight], nyc));
 
   // build a tooltip for our map
-  const tooltip = d3.select("#map").append("div")
+  const tooltip = d3.select(mapId).append("div")
         .attr("class", "tooltip")
         .style("opacity", 1);
 
@@ -119,22 +171,18 @@ buildDesc();
   let dataLength = -1;
   let mapData = [];
 
+  // find out what sort of datatypes we have here
   const floats = [];
   const strings = [];
-
   headers.forEach((header) => {
-    if (covidMapObj[0][header].match(/[a-zA-z]/)) {
-      strings.push(header);
-    } else if(covidMapObj[0][header].match(/\./)) {
-      floats.push(header);
-    }
+    const field = covidMapObj[0][header];
+    if (stringPredicate(field)) { strings.push(header); }
+    else if(decimalPredicate(field)) { floats.push(header); }
   });
-
-  const select = document.getElementById("fields");
-  const range = document.getElementById("date");
 
   // prep the method to do the actual drawing
   const draw = () => {
+
     const header = select.options[select.selectedIndex].value;
     const value = range.value;
     covidMapDataChunk.select(value);
@@ -149,15 +197,16 @@ buildDesc();
       ptr = covidMapDataChunk.expose_key_int(header);
     }
 
-    // if the data memory buffer needs to be changed at all
+    // check if the data memory buffer needs to be changed at all
     const length = covidMapDataChunk.length();
-    console.log(length, dataLength);
     if ( dataPointer !== ptr || dataLength != length )  {
-      console.log("Creating new array buffer!");
+
+      console.log(`Creating new array buffer! pointer at ${ptr} w/ length ${length}`);
       mapData = new Uint32Array(memory.buffer, ptr, length);
       dataPointer = ptr;
       dataLength = length;
     }
+
 
     // sometimes we need to transform the data coming out
     // this line totally breaks the map though
@@ -172,15 +221,16 @@ buildDesc();
 
     console.log(mapData, ptr, covidMapDataChunk.keys(), mapDataMin, mapDataMax);
 
-    document.getElementById('min').innerText = mapDataMin;
-    document.getElementById('max').innerText = mapDataMax;
-    document.getElementById('sum').innerText = `Total: ${mapDataSum}`;
-    document.getElementById('avg').innerText = `Average: ${Math.floor(mapDataAvg * 100) / 100}`;
+    min.innerText = mapDataMin;
+    max.innerText = mapDataMax;
+    sum.innerText = sumText(mapDataSum);
+    avg.innerText = averageText(Math.floor(mapDataAvg * 100) / 100);
+    date.innerText = dateText(getDateFromCommit(historyData[value]));
 
     const color = (d, i) => {
-      if(d.properties.MODZCTA == 99999) { return '#3aa573'; }
-      const n = buckets(mapData[i], mapDataMin, mapDataMax, dataVizColors.length);
-      return dataVizColors[n]
+      if(d.properties.MODZCTA == 99999) { return parkColor; }
+      const n = buckets(mapData[i], mapDataMin, mapDataMax, legendColors.length);
+      return legendColors[n]
     }
 
     const t = map.transition()
@@ -195,14 +245,10 @@ buildDesc();
               .attr( "stroke", '#f0f0f0')
               .attr("d", path)
               .on("mouseover", function(d, i) {
-                const record = covidMapObj[i];
                 tooltip.transition()
                 .duration(200)
                 .style("opacity", .9);
-                tooltip.html(`
-                  ${record['BOROUGH_GROUP']}: ${record['NEIGHBORHOOD_NAME']}<br>
-                  ${mapData[i]}
-                `)
+                tooltip.html(mapTooltip(covidMapObj[i], mapData[i]))
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
               })
